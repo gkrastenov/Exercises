484b6b11c1a4cd8b9ed66f5c4b215c23f53563f3
haldenbg <georgi_kr1991@abv.bg>
2019-08-20 22:51:17 +0300

    Adventure app

---
+++ Xamarin/AdventureApp/AdventureApp/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AdventureApp.Models
+{
+    public class User
+    {
+        #region 
+        // values for registration
+        private string firstName;
+        private string lastName;
+        private string password;
+        private string email;
+        #endregion
+
+        private int age;
+        private string phoneNumber;
+        private string country;
+
+        public User(string firstName, string lastName, string password, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.password = password;
+            this.email = email;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+
+
+        public string ReturnFullName()
+        {
+           return this.firstName + " "+ this.lastName;
+        }
+
+    }
+}
