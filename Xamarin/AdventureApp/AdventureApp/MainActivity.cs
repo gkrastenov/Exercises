1f12afc12aa477791f887eb934e7803b699aa5a7
haldenbg <georgi_kr1991@abv.bg>
2019-08-21 21:27:43 +0300

    Updated MainActivity logic

--- Xamarin/AdventureApp/AdventureApp/MainActivity.cs
+++ Xamarin/AdventureApp/AdventureApp/MainActivity.cs
@@ -3,17 +3,36 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Support.Design.Widget;
 
 namespace AdventureApp
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "Adventure", MainLauncher = true)]
+    public class MainActivity : Activity
     {
+        Button loginButton;
+        Button registerButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+          
+            loginButton = FindViewById<Button>(Resource.Id.btn_Login);
+            registerButton = FindViewById<Button>(Resource.Id.btn_Register);          
+            loginButton.Click += LoginButton_Click;
+            registerButton.Click += RegisterButton_Click;
+
+        }
+        private void RegisterButton_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));
+        }
+
+        private void LoginButton_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(LoginActivity));
         }
     }
 }
