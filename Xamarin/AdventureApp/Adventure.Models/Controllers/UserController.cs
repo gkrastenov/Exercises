1e340566ec8d0354e2d662b7ac5abc5e1a08b185
haldenbg <georgi_kr1991@abv.bg>
2019-08-21 21:23:14 +0300

    Create User model,controller and empty folder for Validations

---
+++ Xamarin/AdventureApp/Adventure.Models/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿namespace Adventure.Controllers
+{
+    using AdventureApp.Models;
+    using System;
+
+    public class UserController
+    {
+        private User currentUser;
+
+        public UserController(User newUser)
+        {
+            this.currentUser = newUser;
+            RegisterNewUser(newUser);
+        }
+
+        public string GetFullName()
+        {
+            return currentUser.FirstName + " " + currentUser.LastName;
+        }
+        public string GetAge()
+        {
+            return currentUser.Age;
+        }
+        public string GetCountry()
+        {
+            return currentUser.Country;
+        }
+        public string GetPhoneNumber()
+        {
+            return currentUser.PhoneNumber;
+        }
+        private void RegisterNewUser(User newUser)
+        {
+           // TODO : method for Register new User;
+        }
+
+    }
+}
---
+++ Xamarin/AdventureApp/Adventure.Models/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AdventureApp.Models
+{
+    public class User
+    {
+        #region 
+        // values for registration
+        private string username;
+        private string password;
+        private string email;
+        #endregion
+        /*
+        private string firstName;
+        private string lastName;
+        private string age;
+        private string phoneNumber;
+        private string country;
+        */
+        public User(string username, string password, string email)
+        {
+            this.username = username;
+            this.password = password;
+            this.email = email;
+        }
+        public User()
+        {
+            // constructor for UserController
+        }
+        public string Username { get => this.username; }
+        public string Password { get => this.password; }
+        public string Email { get => this.email; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+
+
+
+    }
+}
